7ac7da199795833d10f74c0db2611c9a45c38b82
Michael V. O'Brien <michael@michaelobrien.info>
2011-05-19 19:11:27 +0900

    Downgrade Tritonn MySQL to 1.0.12.

--- Library/Formula/tritonn-mysql.rb
+++ Library/Formula/tritonn-mysql.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class TritonnMysql <Formula
-  version '1.0.12'
-  url 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Ftritonn%2F44474%2Ftritonn-1.0.12-mysql-5.0.87.tar.gz'
+  version '1.0.8'
+  url 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Ftritonn%2F28539%2Fmysql-5.0.51-tritonn-1.0.8.tar.gz'
   homepage 'http://qwik.jp/tritonn/about_en.html'
-  md5 'b7a8805ca1e6c31f515a5a5060b33098'
+  md5 '6df954a4fa77eb63671dff7770f8f733'
 
   depends_on 'pkg-config' => :build
   depends_on 'mecab'
